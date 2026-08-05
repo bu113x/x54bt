7cc523888c25d668dcf25578ec6eb11f327aa8f7
Ahmad <rabiu2993@gmail.com>
2026-08-05 15:27:19 +0100

    chore: log redirexcto link on reset password.

--- src/app/[locale]/(auth)/forgot-password/page.tsx
+++ src/app/[locale]/(auth)/forgot-password/page.tsx
@@ -20,13 +20,15 @@ const ForgotPassword = () => {
 
     const formData = new FormData(e.currentTarget);
     const email = String(formData.get("email") ?? "");
+    const redirectTo = `${process.env.NEXT_PUBLIC_APP_URL}/auth/callback?next=/reset-password`;
+    console.log("Redirecting to:", redirectTo);
 
     setIsSubmitting(true);
     const supabase = createClient();
     const { error: forgotError } = await supabase.auth.resetPasswordForEmail(
       email,
       {
-        redirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/auth/callback?next=/reset-password`,
+        redirectTo,
       },
     );
     setIsSubmitting(false);
